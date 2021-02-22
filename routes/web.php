c4ecd9cf005c37a7dbbd8372c7fd22744a8db40d
salvoab <salvatore.abate85@gmail.com>
2021-02-22 16:43:02 +0100

    Creato modello Student con artisan make:model -a
    Implementata la migrazione e inserito attributo fillable nella classe Student

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PageController@index');
+Route::get('/', 'PageController@index')->name('home');
+Route::resource('students', 'StudentController');
